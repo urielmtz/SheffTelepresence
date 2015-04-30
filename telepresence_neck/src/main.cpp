394ca10578e2a03c8d8734ae5a1b43fb8f0f4948
urielmtz <uriel.marherz@gmail.com>
2015-04-30 23:59:59 +0100

    first commit of control of the icub neck for telepresence

---
+++ telepresence_neck/src/main.cpp
@@ -0,0 +1,193 @@
+/*
+* Copyright: (C) 2015 Sheffield Robotics
+* Authors: Uriel Martinez
+* CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+*/
+
+#include <stdio.h>
+#include <iostream>
+
+// Include the OculusVR SDK
+#include "OVR_CAPI.h"
+
+// Include YARP
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+#include <yarp/dev/all.h>
+
+using namespace std;
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::dev;
+
+
+class OculusModule: public RFModule
+{
+    private:
+        ovrHmd hmd;
+        bool OculusDeviceReady;
+
+		Port output;
+		Port confOutput;
+
+		int nsample;
+
+        float yaw;
+        float eyePitch;
+        float eyeRoll;
+        float wParam;
+
+    public:
+        OculusModule()
+        {
+            OculusDeviceReady = false;
+            cout << "Running OculusModule" << endl;
+			nsample = 0;
+        }
+
+        ~OculusModule(){}
+
+        bool updateModule()
+        {
+            cout << "Waiting for device..." << endl;
+            OculusDeviceReady = Initialization();
+
+            if( OculusDeviceReady )
+            {
+                // Get more details about the HMD.
+                ovrSizei resolution = hmd->Resolution;
+                // Start the sensor which provides the Rift’s pose and motion.
+                ovrHmd_ConfigureTracking(hmd, ovrTrackingCap_Orientation | ovrTrackingCap_MagYawCorrection | ovrTrackingCap_Position, 0);
+                // Query the HMD for the current tracking state.
+                ovrTrackingState ts = ovrHmd_GetTrackingState(hmd, ovr_GetTimeInSeconds());
+                
+                if(ts.StatusFlags & (ovrStatus_OrientationTracked | ovrStatus_PositionTracked))
+                {
+                    ovrPosef pose = ts.HeadPose.ThePose;
+                    // pose.Orientation.GetEurlerAngles<Axis_Y, Aixs_X, Axis_Z>(&yaw, &eyePitch, &eyeRoll);
+                    yaw = pose.Orientation.y;
+                    eyePitch = pose.Orientation.x;
+                    eyeRoll = pose.Orientation.z;
+                    wParam = pose.Orientation.w;
+
+                    cout << "POSE [y,x,z => yaw,pitch,roll]: " << yaw << " " << eyePitch << " " << eyeRoll << endl;
+                    toEuler(&yaw, &eyePitch, &eyeRoll, wParam);
+                    cout << "POSE Euler [yaw,pitch,roll]: " << yaw << " " << eyePitch << " " << eyeRoll << endl;
+
+                    nsample = 0;
+                    Bottle bot;
+                    bot.addString("abs");
+                    bot.addDouble(yaw*-180);		// (y) yaw in oculus which is x in icub
+                    bot.addDouble(eyePitch*180);	// (x) pitch in oculus which is y in icub
+                    bot.addDouble(0);
+                    output.write(bot);
+                    // printf("Sent message: %s\n", bot.toString().c_str());
+                }
+            }
+
+            return true;
+        }
+
+        bool configure(ResourceFinder &)
+        {
+			output.open("/telepresence/neck:o");
+			confOutput.open("/telepresence/neckconf:o");
+			
+		    // Network::connect("/telepresence/neck:o","/iKinGazeCtrl/angles:i");
+
+            while( !Network::isConnected("/telepresence/neckconf:o","/iKinGazeCtrl/rpc") )
+    		    Network::connect("/telepresence/neckconf:o","/iKinGazeCtrl/rpc");
+
+		    Bottle bot;
+		    bot.addString("bind");
+		    bot.addString("roll");
+		    bot.addDouble(-0.01);
+		    bot.addDouble(0.01);
+		    confOutput.write(bot);
+		    printf("Sent message: %s\n", bot.toString().c_str());
+
+            return true;
+        }
+
+        bool interruptModule()
+        {
+            Network::disconnect("/telepresence/neck:o","/iKinGazeCtrl/angles:i");
+            Network::disconnect("/telepresence/neckconf:o","/iKinGazeCtrl/rpc");
+
+            // Do something with the HMD.
+            ovrHmd_Destroy(hmd);
+            ovr_Shutdown();
+
+			output.close();
+
+            return true;
+        }
+
+        double getPeriod()
+        {
+            return 0.03;
+        }
+
+        bool Initialization()
+        {
+            ovr_Initialize();
+            hmd = ovrHmd_Create(0);
+            
+            if(hmd)
+                return true; 
+            else
+                return false;             
+        }
+
+        void toEuler(float *x, float *y, float *z, float w)
+        {
+            float s=sin(w);
+            float c=cos(w);
+            float t=1-c;
+            float ap, bp, cp;
+
+            if (((*x)*(*y)*t + (*z)*s) > 0.998)
+            { // north pole singularity detected
+                ap = 0;
+                bp = 2*atan2((*x)*sin(w/2),cos(w/2));
+                cp = M_PI/2;
+            }
+            else if (((*x)*(*y)*t + (*z)*s) < -0.998)
+            { // south pole singularity detected
+                ap = 0;
+                bp = -2*atan2((*x)*sin(w/2),cos(w/2));
+                cp = -M_PI/2;
+            }
+            else
+            {
+                ap = atan2((*x) * s - (*y) * (*z) * t , 1 - ((*x)*(*x) + (*z)*(*z)) * t);
+                bp = atan2((*y) * s- (*x) * (*z) * t , 1 - ((*y)*(*y)+ (*z)*(*z) ) * t);
+                cp = asin((*x) * (*y) * t + (*z) * s) ;
+            }
+
+            *x = ap;
+            *y = bp;
+            *z = cp;
+        }
+};
+
+int main(int argc, char **argv)
+{
+    Network yarp;
+
+    if( !yarp.checkNetwork() )
+    {
+        cout << "yarp server not found..." << endl;
+        return 1;
+    }
+
+    ResourceFinder rf;
+    rf.setVerbose(true);
+    rf.setDefaultContext("oculusRiftModule");
+    rf.setDefaultConfigFile("config.ini");
+    rf.configure(argc,argv);
+    
+    OculusModule mod;
+
+    return mod.runModule(rf);
+}
