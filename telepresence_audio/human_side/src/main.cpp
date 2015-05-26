74de91fc0516b82bc2f6efb2c6fc1bf7153e928c
urielmtz <uriel.marherz@gmail.com>
2015-05-26 10:13:20 +0100

    update of telepresence audio to resource modules

--- telepresence_audio/human_side/src/main.cpp
+++ telepresence_audio/human_side/src/main.cpp
@@ -4,14 +4,16 @@
 * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 */
 
+#include <iostream>
 #include <stdio.h>
-#include <yarp/dev/PolyDriver.h>
-#include <yarp/dev/AudioGrabberInterfaces.h>
-#include <yarp/os/Property.h>
 #include <yarp/os/Network.h>
-#include <yarp/os/Port.h>
 #include <yarp/os/Time.h>
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+#include <yarp/dev/all.h>
 
+using namespace std;
+using namespace yarp;
 using namespace yarp::os;
 using namespace yarp::sig;
 using namespace yarp::dev;
@@ -20,75 +22,111 @@ const int rec_seconds = 1;
 const int sample_block = 8192;
 const int sample_rate = 8000;
 
+
+class AudioHumanModule: public RFModule
+{
+    private:
+        Port senderPort;
+        BufferedPort<Sound> receiverPort;
+        IAudioGrabberSound *get;
+        Property receiverConf;
+        Property senderConf;
+        IAudioRender *put;
+
+        Sound sendSound;
+        Sound *receiveSound;
+
+    public:
+        AudioHumanModule(){}
+
+        ~AudioHumanModule(){}
+
+        bool updateModule()
+        {
+            double t1=yarp::os::Time::now();
+            get->getSound(sendSound);
+            double t2=yarp::os::Time::now();
+            printf("acquired %f seconds\n", t2-t1);
+            senderPort.write(sendSound);
+
+            receiveSound = receiverPort.read(false);
+            if (receiveSound!=NULL)
+                put->renderSound(*receiveSound);
+
+            return true;
+        }
+
+        bool configure(ResourceFinder &rf)
+        {
+            senderPort.open("/telepresence/audio/human:o");
+            receiverPort.open("/telepresence/audio/human:i");
+
+            senderConf.put("device","portaudio");
+            senderConf.put("read", "");
+            senderConf.put("samples", sample_block*rec_seconds);
+            senderConf.put("rate", sample_rate);
+            PolyDriver senderPoly(senderConf);
+
+            // Make sure we can read sound
+            senderPoly.view(get);
+
+            if (get==NULL)
+            {
+                printf("cannot open read device interface\n");
+                return 1;
+            }
+
+
+            // Receiver: Get an audio write device.
+            receiverConf.put("device","portaudio");
+            receiverConf.put("samples", sample_rate);
+            receiverConf.put("write", "1");
+            PolyDriver receiverPoly(receiverConf);
+            // Make sure we can write sound
+            receiverPoly.view(put);
+
+            if (put==NULL)
+            {
+                printf("cannot open write device interface\n");
+                return 1;
+            }
+
+            get->startRecording(); //this is optional, the first get->getsound() will do this anyway.
+
+            return true;
+        }
+
+        bool interruptModule()
+        {
+            get->stopRecording(); //stops recording.
+
+            return true;
+        }
+
+        double getPeriod()
+        {
+            return 0.001;
+        }
+};
+
 int main(int argc, char *argv[])
 {
-    // Open the network
     Network yarp;
-    Port senderPort;
-    BufferedPort<Sound> receiverPort;
 
-    senderPort.open("/telepresence/human:o");
-    receiverPort.open("/telepresence/human:i");
-
-    // Sender: Get a portaudio read device.
-    Property senderConf;
-    senderConf.put("device","portaudio");
-    senderConf.put("read", "");
-    senderConf.put("samples", sample_block*rec_seconds);
-//    senderConf.put("samples", 44100*rec_seconds);
-//    senderConf.put("rate", 16000);
-    senderConf.put("rate", sample_rate);
-    PolyDriver senderPoly(senderConf);
-    IAudioGrabberSound *get;
-    // Make sure we can read sound
-    senderPoly.view(get);
-
-    if (get==NULL)
+    if (!yarp.checkNetwork())
     {
-        printf("cannot open read device interface\n");
-        return 1;
+        cout << endl << "ERROR: yarpserver not found" << endl;
+        return false;
     }
 
+    ResourceFinder rf;
+    rf.setVerbose(true);
+    rf.setDefaultContext("AudioHumanModule");
+    rf.setDefaultConfigFile("config.ini");
+    rf.configure(argc,argv);
+    
+    AudioHumanModule mod;
 
-    // Receiver: Get an audio write device.
-    Property receiverConf;
-    receiverConf.put("device","portaudio");
-    receiverConf.put("samples", sample_rate);
-//    conf.put("samples", "8192");
-    receiverConf.put("write", "1");
-    PolyDriver receiverPoly(receiverConf);
-    IAudioRender *put;
-    // Make sure we can write sound
-    receiverPoly.view(put);
-
-    if (put==NULL)
-    {
-        printf("cannot open write device interface\n");
-        return 1;
-    }
-
-    //Grab and send
-    Sound sendSound;
-
-    //Receive and render
-    Sound *receiveSound;
-
-    get->startRecording(); //this is optional, the first get->getsound() will do this anyway.
-
-    while (true)
-    {
-        double t1=yarp::os::Time::now();
-        get->getSound(sendSound);
-        double t2=yarp::os::Time::now();
-        printf("acquired %f seconds\n", t2-t1);
-        senderPort.write(sendSound);
-
-        receiveSound = receiverPort.read(false);
-        if (receiveSound!=NULL)
-            put->renderSound(*receiveSound);
-    }
-
-    get->stopRecording(); //stops recording.
-    return 0;
+    return mod.runModule(rf);    
 }
 
--- telepresence_audio/robot_side/src/main.cpp
+++ telepresence_audio/robot_side/src/main.cpp
@@ -4,14 +4,16 @@
 * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
 */
 
+#include <iostream>
 #include <stdio.h>
-#include <yarp/dev/PolyDriver.h>
-#include <yarp/dev/AudioGrabberInterfaces.h>
-#include <yarp/os/Property.h>
 #include <yarp/os/Network.h>
-#include <yarp/os/Port.h>
 #include <yarp/os/Time.h>
+#include <yarp/os/all.h>
+#include <yarp/sig/all.h>
+#include <yarp/dev/all.h>
 
+using namespace std;
+using namespace yarp;
 using namespace yarp::os;
 using namespace yarp::sig;
 using namespace yarp::dev;
@@ -20,74 +22,143 @@ const int rec_seconds = 1;
 const int sample_block = 8192;
 const int sample_rate = 8000;
 
+
+class AudioRobotModule: public RFModule
+{
+    private:
+        Port senderPort;
+        BufferedPort<Sound> receiverPort;
+        IAudioGrabberSound *get;
+        Property receiverConf;
+        Property senderConf;
+        IAudioRender *put;
+
+        Sound sendSound;
+        Sound *receiveSound;
+
+        Contact gtw_contactAudioInputPort;
+        Contact gtw_contactAudioOutputPort;
+
+        string ip_address;
+
+    public:
+        AudioRobotModule(){}
+
+        ~AudioRobotModule(){}
+
+        bool updateModule()
+        {
+            double t1=yarp::os::Time::now();
+            get->getSound(sendSound);
+            double t2=yarp::os::Time::now();
+            printf("acquired %f seconds\n", t2-t1);
+            senderPort.write(sendSound);
+
+            receiveSound = receiverPort.read(false);
+            if (receiveSound!=NULL)
+                put->renderSound(*receiveSound);
+
+            return true;
+        }
+
+        bool configure(ResourceFinder &rf)
+        {
+            Property config;
+            config.fromConfigFile(rf.getContext() + "config.ini");
+            
+            Bottle &bGeneral = config.findGroup("host_computer");
+
+            ip_address = bGeneral.find("ip_address").asString().c_str();
+            cout << "Host computer ip address: " << ip_address << endl;
+
+            gtw_contactAudioInputPort = gtw_contactAudioInputPort.addName("/telepresence/audio/robot:i");
+            gtw_contactAudioInputPort = gtw_contactAudioInputPort.addCarrier("tcp");
+            gtw_contactAudioInputPort = gtw_contactAudioInputPort.addHost(ip_address);
+            gtw_contactAudioInputPort = gtw_contactAudioInputPort.addPort(80000);
+
+            gtw_contactAudioOutputPort = gtw_contactAudioOutputPort.addName("/telepresence/audio/robot:o");
+            gtw_contactAudioOutputPort = gtw_contactAudioOutputPort.addCarrier("tcp");
+            gtw_contactAudioOutputPort = gtw_contactAudioOutputPort.addHost(ip_address);
+            gtw_contactAudioOutputPort = gtw_contactAudioOutputPort.addPort(80001);
+
+            Network::registerContact(gtw_contactAudioInputPort);
+            Network::registerContact(gtw_contactAudioOutputPort);
+
+            senderPort.open(gtw_contactAudioOutputPort);
+            receiverPort.open(gtw_contactAudioInputPort);
+
+            senderConf.put("device","portaudio");
+            senderConf.put("read", "");
+            senderConf.put("samples", sample_block*rec_seconds);
+            senderConf.put("rate", sample_rate);
+            PolyDriver senderPoly(senderConf);
+
+            // Make sure we can read sound
+            senderPoly.view(get);
+
+            if (get==NULL)
+            {
+                printf("cannot open read device interface\n");
+                return 1;
+            }
+
+
+            // Receiver: Get an audio write device.
+            receiverConf.put("device","portaudio");
+            receiverConf.put("samples", sample_rate);
+            receiverConf.put("write", "1");
+            PolyDriver receiverPoly(receiverConf);
+            // Make sure we can write sound
+            receiverPoly.view(put);
+
+            if (put==NULL)
+            {
+                printf("cannot open write device interface\n");
+                return 1;
+            }
+
+            get->startRecording(); //this is optional, the first get->getsound() will do this anyway.
+
+            return true;
+        }
+
+        bool interruptModule()
+        {
+            get->stopRecording(); //stops recording.
+
+            Network::unregisterContact(gtw_contactAudioInputPort);
+            Network::unregisterContact(gtw_contactAudioOutputPort);
+
+            senderPort.close();
+            receiverPort.close();
+
+            return true;
+        }
+
+        double getPeriod()
+        {
+            return 0.001;
+        }
+};
+
 int main(int argc, char *argv[])
 {
-    // Open the network
     Network yarp;
-    Port senderPort;
-    BufferedPort<Sound> receiverPort;
 
-    senderPort.open("/telepresence/robot:o");
-    receiverPort.open("/telepresence/robot:i");
-
-    // Sender: Get a portaudio read device.
-    Property senderConf;
-    senderConf.put("device","portaudio");
-    senderConf.put("read", "");
-//    senderConf.put("samples", 11025*rec_seconds);
-    senderConf.put("samples", sample_block*rec_seconds);
-    senderConf.put("rate", sample_rate);
-    PolyDriver senderPoly(senderConf);
-    IAudioGrabberSound *get;
-    // Make sure we can read sound
-    senderPoly.view(get);
-
-    if (get==NULL)
+    if (!yarp.checkNetwork())
     {
-        printf("cannot open read device interface\n");
-        return 1;
+        cout << endl << "ERROR: yarpserver not found" << endl;
+        return false;
     }
 
+    ResourceFinder rf;
+    rf.setVerbose(true);
+    rf.setDefaultContext("/home/uriel/Packages/SheffTelepresence/telepresence_audio/conf/");
+    rf.setDefaultConfigFile("config.ini");
+    rf.configure(argc,argv);
+    
+    AudioRobotModule mod;
 
-    // Receiver: Get an audio write device.
-    Property receiverConf;
-    receiverConf.put("device","portaudio");
-    receiverConf.put("samples", sample_rate);
-//    conf.put("samples", "8192");
-    receiverConf.put("write", "1");
-    PolyDriver receiverPoly(receiverConf);
-    IAudioRender *put;
-    // Make sure we can write sound
-    receiverPoly.view(put);
-
-    if (put==NULL)
-    {
-        printf("cannot open write device interface\n");
-        return 1;
-    }
-
-    //Grab and send
-    Sound sendSound;
-
-    //Receive and render
-    Sound *receiveSound;
-
-    get->startRecording(); //this is optional, the first get->getsound() will do this anyway.
-
-    while (true)
-    {
-        double t1=yarp::os::Time::now();
-        get->getSound(sendSound);
-        double t2=yarp::os::Time::now();
-        printf("acquired %f seconds\n", t2-t1);
-        senderPort.write(sendSound);
-
-        receiveSound = receiverPort.read(false);
-        if (receiveSound!=NULL)
-            put->renderSound(*receiveSound);
-    }
-
-    get->stopRecording(); //stops recording.
-    return 0;
+    return mod.runModule(rf);    
 }
 
